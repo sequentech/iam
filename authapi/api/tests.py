5c4bf6fbc286ea35e8360739ea8e8fea2c03bfaa
Daniel Garcia Moreno <danigm@wadobo.com>
2014-11-27 19:22:52 +0100

    Added Test view and test_api

--- authapi/api/tests.py
+++ authapi/api/tests.py
@@ -1,3 +1,19 @@
+import json
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class ApiTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_api(self):
+        c = Client()
+        data = {'username': 'john', 'password': 'smith'}
+        response = c.post('/api/test/', data)
+
+        self.assertEqual(response.status_code, 200)
+        r = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(r['status'], 'ok')
+        self.assertEqual(r['post']['username'][0], 'john')
+        self.assertEqual(r['post']['password'][0], 'smith')
--- authapi/api/views.py
+++ authapi/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+import json
 
-# Create your views here.
+
+class Test(View):
+    ''' Test view that returns the response data '''
+
+    def get(self, request):
+        data = {'status': 'ok', 'method': 'GET'}
+        data['get'] = dict(request.GET)
+        jsondata = json.dumps(data)
+        return HttpResponse(jsondata, content_type='application/json')
+
+    def post(self, request):
+        data = {'status': 'ok', 'method': 'POST'}
+        data['post'] = dict(request.POST)
+        jsondata = json.dumps(data)
+        return HttpResponse(jsondata, content_type='application/json')
+test = Test.as_view()
