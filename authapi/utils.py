0f9d81cd8da4c864e8347be5538b1ea55f43ebef
Daniel Garcia Moreno <danigm@wadobo.com>
2015-01-27 13:36:24 +0100

    Using the username to show as sender

--- authapi/utils.py
+++ authapi/utils.py
@@ -195,7 +195,7 @@ def send_code(user, templ=None):
         email = EmailMessage(
             conf.get('subject'),
             msg,
-            settings.DEFAULT_FROM_EMAIL,
+            acl.user.user.username + '<' + settings.DEFAULT_FROM_EMAIL + '>',
             [receiver],
             headers = {'Reply-To': acl.user.user.email}
         )
