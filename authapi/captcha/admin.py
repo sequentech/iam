c8b8d37f4498fcc33b7ecc6ad2c6c3408249823b
Victor Ramirez <virako@wadobo.com>
2015-02-17 11:50:57 +0100

    Added captcha to django admin

--- authapi/captcha/admin.py
+++ authapi/captcha/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Captcha
 
-# Register your models here.
+
+class CaptchaAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Captcha, CaptchaAdmin)
