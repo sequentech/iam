4eb6988950094744d6cc028eeb64143e94d968fb
Eduardo Robles Elvira <edulix@wadobo.com>
2015-03-03 23:40:16 +0100

    only showing django admin on debug mode

--- authapi/authapi/urls.py
+++ authapi/authapi/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^api/', include('api.urls')),
-    url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+      url(r'^admin/', include(admin.site.urls)),
+    )
