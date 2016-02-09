20fd34ff87d11e47c69719c9da5a840da04dfe94
Eduardo Robles Elvira <edulix@agoravoting.com>
2016-02-09 20:35:50 +0100

    improve plugins call function: it should not mask exceptions and added some doc

--- authapi/plugins.py
+++ authapi/plugins.py
@@ -2,14 +2,24 @@ from django.conf import settings
 
 
 def call(func, *args):
+    '''
+    Method to implement extension points in the code of authapi. It will
+    call the corresponding function by name (func) with the given args in each
+    plugin.
+
+    Note: it does not mask exceptions: if the plugin function raises an
+    exception, it will raise the exception.
+    '''
     res = []
     for plugin in settings.PLUGINS:
         views = __import__(plugin + '.views', fromlist=[''])
-        try:
-            method = getattr(views, func)
-            aux_res = method(*args)
-            if aux_res:
-                res.append(aux_res)
-        except:
+
+        # check that this plugin has this extension point, or continue otherwise
+        if not hasattr(views, func):
             continue
+        method = getattr(views, func)
+
+        aux_res = method(*args)
+        if aux_res:
+            res.append(aux_res)
     return res
