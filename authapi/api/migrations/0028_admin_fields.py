534811ab43c2e4b40f91f349104439f8215e9b82
Findeton <felix@nvotes.com>
2017-10-31 17:27:10 -0500

    fix migration

--- authapi/api/migrations/0028_admin_fields.py
+++ authapi/api/migrations/0028_admin_fields.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='userdata',
+            model_name='authevent',
             name='admin_fields',
             field=jsonfield.fields.JSONField(blank=True, db_index=True, default=[], max_length=4096, null=True),
             preserve_default=True,
