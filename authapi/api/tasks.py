70e0c372da0ae74fec53ef2551507d9e13382fdf
Eduardo Robles Elvira <edulix@nvotes.com>
2020-03-13 12:22:58 +0100

    working on children elections

--- authapi/api/tasks.py
+++ authapi/api/tasks.py
@@ -14,6 +14,7 @@
 # along with authapi.  If not, see <http://www.gnu.org/licenses/>.
 
 import requests
+import json
 from djcelery import celery
 from django.conf import settings
 from django.core.mail import send_mail
