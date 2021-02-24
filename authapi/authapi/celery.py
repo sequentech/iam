b27adc3843fd9267e1c8c5353162a82eac798707
Eduardo Robles Elvira <edulix@nvotes.com>
2021-02-24 13:22:23 +0100

    updating celery config to new config version (#117)
    * updating celery config to new config version
    
    * update celery config on test_settings too
    
    * trying to fix broker backend in tests

--- authapi/authapi/celery.py
+++ authapi/authapi/celery.py
@@ -1,17 +1,14 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authapi.settings')
 
 app = Celery('authapi')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object(settings.CELERY_CONFIG)
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
