2dd40beb4395c5e35dbc8bdb90fad493f46c6ada
Eduardo Robles Elvira <edulix@gmail.com>
2021-10-27 07:35:18 +0200

    userdata.tlf should have priority in output to metadata.tlf

--- authapi/api/models.py
+++ authapi/api/models.py
@@ -605,7 +605,8 @@ class UserData(models.Model):
                     metadata = json.loads(metadata)
             else:
                 metadata = self.metadata
-            d.update(metadata)
+            metadata.update(d)
+            d = metadata
         return d
 
     def __str__(self):
