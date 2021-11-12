ffdfb5efa80e1705d984b190d39c3c035b0e1623
Eduardo Robles <edulix@nvotes.com>
2021-11-12 09:42:15 +0100

    allowing more powerful reports, counting not only votes but things like active users and other custom data (#150)

--- authapi/api/management/commands/send_report.py
+++ authapi/api/management/commands/send_report.py
@@ -19,6 +19,7 @@
 from api.models import AuthEvent
 
 from django.utils import timezone
+from django.db.models import Count
 from django.core.mail import send_mail, EmailMessage
 from django.core.management.base import BaseCommand, CommandError
 import tempfile
@@ -92,7 +93,8 @@ def gen_text(
 #   # this is optional, by default it will be 488 x 130
 #   "logo_size": { "width": 488, "height": 130 },
 
-#   "table_headers": [ "Votación", "Votos", "% censo", "# Electores" ],
+#   "table_headers": [ "Votación", "Medida", "Valor", "% censo", "# Electores" ],
+#   "default_measurement_name": "Votos",
 #   "groups": [
 #       {
 #           "auth_event_ids": [24, 25, 26],
@@ -115,6 +117,13 @@ def gen_text(
 #           "auth_event_ids": [27, 28, 29],
 #           "title": "Candidatos para Consejeros por Sector 2 - adult",
 #           "extra_filters": {"user__metadata__age__gte": 18}
+#       },
+#       {
+#           "auth_event_ids": [27, 28, 29],
+#           "title": "Candidatos para Consejeros por Sector 2 - adult - registrados",
+#           "extra_filters": {"user__metadata__age__gte": 18}
+#           "measurement_name": "Registrados",
+#           "measurement_query_base": "active"
 #       }
 #   ]
 # }
@@ -206,10 +215,17 @@ class Command(BaseCommand):
             for header_text in config['table_headers']
         ])
 
+        default_measurement_name = config.get(
+            'default_measurement_name',
+            'Votes'
+        )
+
         # generate rows
         for group in config['groups']:
             census = 0
-            votes = 0
+            # usually measurement is "Votes", but it can be customized to be
+            # something else
+            measurement_value = 0
             for auth_event_id in group['auth_event_ids']:
                 auth_event = AuthEvent.objects.get(pk=auth_event_id)
 
@@ -220,17 +236,81 @@ class Command(BaseCommand):
                     )
                 census += census_query.count()
 
-                votes_query = auth_event.get_num_votes_query()
-                if 'extra_filters' in group:
-                    votes_query = votes_query.filter(
-                        **group['extra_filters']
+                # Note that because the base query starts from the census query
+                # as it is in the case of measurement_query_base being 'census'
+                # or 'active', we apply the same extra_filters as the
+                # census_query variable.
+                #
+                # When measurement_query_base is 'votes' (the default), the
+                # selected table is different. This makes same extra_filters not
+                # work with the vote counting query even if they do work in the
+                # census query. For this reason, if measurement_query_base is
+                # 'votes' and measurement_extra_filters is set, we don't apply
+                # the extra_filters to the votes query. In all other cases, we
+                # do.
+                measurement_query_base = group.get(
+                    'measurement_query_base',
+                    'votes'
+                )
+                if measurement_query_base == 'census':
+                    measurement_query = auth_event.get_census_query()
+                    if 'extra_filters' in group:
+                        census_query = census_query.filter(
+                            **group['extra_filters']
+                        )
+                    if 'measurement_extra_filters' in group:
+                        measurement_query = measurement_query.filter(
+                            **group['measurement_extra_filters']
+                        )
+                # 'active' means the user has more than 0 actions executed by
+                # the user in the activity log.
+                elif measurement_query_base == 'active':
+                    measurement_query = auth_event.get_census_query()
+
+                    measurement_query = measurement_query\
+                        .annotate(
+                            actions_count=Count("user__user__executed_actions")
+                        )\
+                        .filter(actions_count__gt=0)
+
+                    if 'extra_filters' in group:
+                        census_query = census_query.filter(
+                            **group['extra_filters']
+                        )
+
+                    if 'measurement_extra_filters' in group:
+                        measurement_query = measurement_query.filter(
+                            **group['measurement_extra_filters']
+                        )
+                # meaning the default, 'votes'
+                elif measurement_query_base == 'votes':
+                    measurement_query = auth_event.get_num_votes_query()
+                    if 'measurement_extra_filters' in group:
+                        measurement_query = measurement_query.filter(
+                            **group['measurement_extra_filters']
+                        )
+                    elif 'extra_filters' in group:
+                        measurement_query = measurement_query.filter(
+                            **group['extra_filters']
+                        )
+                else:
+                    raise Exception(
+                        'invalid measurement_query_base: %r' % ( 
+                            measurement_query_base
+                        )
                     )
-                votes += votes_query.count()
+
+                measurement_value += measurement_query.count()
 
             row = [
                 group['title'],
-                "{:,}".format(votes),
-                "{:,.2f}%".format(votes*100.0/census) if census > 0 else "-",
+                group.get('measurement_name', default_measurement_name),
+                "{:,}".format(measurement_value),
+                (
+                    "{:,.2f}%".format(measurement_value*100.0/census) 
+                    if census > 0
+                    else "-"
+                ),
                 "{:,}".format(census),
             ]
 
@@ -243,6 +323,7 @@ class Command(BaseCommand):
             table_elements,
             colWidths=[
                 250,
+                80,
                 60,
                 80,
                 80
