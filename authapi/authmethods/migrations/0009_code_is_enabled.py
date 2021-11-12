38ca2658dc7f3aa049bf2cfebe13fa9f0768d87d
Eduardo Robles Elvira <edulix@gmail.com>
2021-11-12 17:37:08 +0100

    fix typo

--- authapi/authmethods/migrations/0009_code_is_enabled.py
+++ authapi/authmethods/migrations/0009_code_is_enabled.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='Code',
-            name='is_enbled',
+            name='is_enabled',
             field=models.BooleanField(default=True),
         ),
     ]
