075ada775ff40251aec8e6063aaed5c9b1ca8c85
Eduardo Robles Elvira <edu@sequentech.io>
2022-01-17 11:37:33 +0100

    initial unit tests for tasks app should be working now

--- authapi/tasks/tests.py
+++ authapi/tasks/tests.py
@@ -18,9 +18,11 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from api import test_data
-from api.models import AuthEvent
+from api.models import ACL
+from authmethods.models import Code
 from api.tests import JClient, flush_db_load_fixture, parse_json_response
 from tasks.models import Task
+from utils import reproducible_json_dumps, json_response
 
 class TestListTasks(TestCase):
     '''
@@ -39,9 +41,46 @@ class TestListTasks(TestCase):
         admin_user.userdata.save()
         self.admin_user = admin_user
 
+        admin_user_2 = User(
+            username='test_admin2',
+            email=test_data.auth_email_default1['email']
+        )
+        admin_user_2.save()
+        self.admin_user_2 = admin_user_2
+
+        acl = ACL(
+            user=admin_user.userdata,
+            object_type='AuthEvent',
+            perm='edit',
+            object_id=settings.ADMIN_AUTH_ID
+        )
+        acl.save()
+
+        code = Code(
+            user=admin_user.userdata,
+            code=test_data.auth_email_default['code'],
+            auth_event_id=settings.ADMIN_AUTH_ID
+        )
+        code.save()
+
+        self.empty_response = {
+            'status': 'ok',
+            'tasks': [], 
+            'page': 1, 
+            'total_count': 0, 
+            'page_range': [1], 
+            'start_index': 0,
+            'end_index': 0, 
+            'has_next': False, 
+            'has_previous': False
+        }
+
     def test_list_requires_auth(self):
+        '''
+        Check that listing tasks requires the user to be authenticated
+        '''
         client = JClient()
-        response = client.get('/api/task/', {})
+        response = client.get('/api/tasks/', {})
         self.assertEqual(response.status_code, 403)
 
     def test_list_requires_admin_auth(self):
@@ -54,11 +93,97 @@ class TestListTasks(TestCase):
             settings.ADMIN_AUTH_ID,
             test_data.auth_email_default
         )
-        response = client.get('/api/task/', {})
+        response = client.get('/api/tasks/', {})
         self.assertEqual(response.status_code, 200)
 
         self.admin_user.userdata.event_id = None
         self.admin_user.userdata.save()
 
-        response = client.get('/api/task/', {})
+        response = client.get('/api/tasks/', {})
         self.assertEqual(response.status_code, 403)
+
+    def test_list_empty(self):
+        '''
+        Check that an empty list of tasks works as expected
+        '''
+        client = JClient()
+        client.authenticate(
+            settings.ADMIN_AUTH_ID,
+            test_data.auth_email_default
+        )
+        response = client.get('/api/tasks/', {})
+        self.assertEqual(response.status_code, 200)
+        response_data = parse_json_response(response)
+        self.assertEqual(
+            reproducible_json_dumps(response_data),
+            reproducible_json_dumps(self.empty_response)
+        )
+
+    def test_list_empty2(self):
+        '''
+        Check that tasks for other users do not list in current user
+        '''
+        # create a task for self.admin_user_2, should not appear listed for
+        # self.admin_user
+        task = Task(
+            executer=self.admin_user_2,
+            status='created'
+        )
+        task.save()
+
+        client = JClient()
+        client.authenticate(
+            settings.ADMIN_AUTH_ID,
+            test_data.auth_email_default
+        )
+        response = client.get('/api/tasks/', {})
+        self.assertEqual(response.status_code, 200)
+        response_data = parse_json_response(response)
+        self.assertEqual(
+            reproducible_json_dumps(response_data),
+            reproducible_json_dumps(self.empty_response)
+        )
+
+    def test_list_one(self):
+        '''
+        Check that a list with one task from current user works as expected
+        '''
+        task = Task(
+            executer=self.admin_user,
+            status='created'
+        )
+        task.save()
+
+        client = JClient()
+        client.authenticate(
+            settings.ADMIN_AUTH_ID,
+            test_data.auth_email_default
+        )
+        response = client.get('/api/tasks/', {})
+        self.assertEqual(response.status_code, 200)
+        response_data = parse_json_response(response)
+        expected_response = {
+            'status': 'ok',
+            'tasks': [
+                {
+                    'id': 2, 
+                    'executer_username': 'test_admin',
+                    'status': 'created', 
+                    'metadata': {}, 
+                    'name': '', 
+                    'input': {},
+                    'output': {}
+                }
+            ], 
+            'page': 1, 
+            'total_count': 1, 
+            'page_range': [1], 
+            'start_index': 1,
+            'end_index': 1, 
+            'has_next': False, 
+            'has_previous': False
+        }
+        self.assertEqual(
+            reproducible_json_dumps(response_data),
+            reproducible_json_dumps(expected_response)
+        )
