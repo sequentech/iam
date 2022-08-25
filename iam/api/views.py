fa51c014b464154caf53d74b5ec716665ddfb21b
Findeton <felix@sequentech.io>
2022-08-25 16:01:03 -0500

    fix

--- iam/api/views.py
+++ iam/api/views.py
@@ -2161,7 +2161,7 @@ class CensusSendAuth(View):
             config = {}
             if req.get('msg', ''):
                 config['msg'] = req.get('msg', '')
-            if req.get('html_message', ''):
+            if req.get('html_message', None):
                 config['html_message'] = req.get('html_message', '')
             if req.get('subject', ''):
                 config['subject'] = req.get('subject', '')
