b06a781e8669db80608acf091e4e3b68fde263ae
Eduardo Robles Elvira <edu@sequentech.io>
2023-05-30 13:18:46 +0200

    fix broken unit test

--- iam/api/tests.py
+++ iam/api/tests.py
@@ -1149,6 +1149,7 @@ class TestAuthEvent(TestCase):
         auth_event = {
             'events': {
                 'allow_public_census_query': False,
+                'alternative_auth_methods': None,
                 'auth_method': 'email',
                 'created': '2018-03-29T11:20:30.656486+00:00',
                 'auth_method_config': test_data.ae_email_default__method_config,
