2fcb98f37a87efadc5d01723702531e4d309472d
Findeton <felix@sequentech.io>
2024-06-24 20:08:05 -0500

    wip

--- iam/api/views.py
+++ iam/api/views.py
@@ -796,7 +796,7 @@ class Ping(View):
             }
             auth_event = get_object_or_404(AuthEvent, pk=pk)
             req = {}
-            auth_data = return_auth_data('Ping', req, request, user, auth_event)
+            auth_data = return_auth_data('Ping', req, request, u, auth_event)
             if 'vote-permission-token' in auth_data:
                 data['vote-permission-token'] = auth_data['vote-permission-token']
             if 'vote-children-info' in auth_data:
