45c7efd6ca3397407c9de846e4e9c4482589aeb5
Findeton <felix@sequentech.io>
2025-12-04 16:53:37 -0500

    wip

--- iam/api/views.py
+++ iam/api/views.py
@@ -3047,7 +3047,7 @@ class TallyStatusView(View):
 
         # get AuthEvent and parse request json
         req = parse_json_request(request)
-        TallyStatusView.tally_status_post(pk, req, request.user)
+        return TallyStatusView.tally_status_post(pk, req, request.user)
         
 
     def get(self, request, pk):
